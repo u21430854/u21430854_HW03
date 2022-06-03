e5aebf57e4d0382515c394bcaf424997c0531bcb
u21430854 <u21430854@tuks.co.za>
2022-06-03 10:18:36 +0200

    Added all necessary views, controllers and models. Not much else.

--- u21430854_HW03/Controllers/FilesController.cs
+++ u21430854_HW03/Controllers/FilesController.cs
@@ -3,18 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System.IO;
 
 namespace u21430854_HW03.Controllers
 {
-    public class HomeController : Controller
+    public class FilesController : Controller
     {
+        // GET: Files
         public ActionResult Index()
-        {
-            return View();
-        }
-
-        public ActionResult AboutMe()
         {
             return View();
         }
---
+++ u21430854_HW03/Models/FileModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace u21430854_HW03.Models
+{
+    public class FileModel
+    {
+        public string FileName { get; set; }
+
+        public HttpPostedFileBase Files { get; set; }
+    }
+}
