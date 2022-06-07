f5b79451b9f521598aab78bd46e3191503d732fd
u21430854 <u21430854@tuks.co.za>
2022-06-07 19:46:32 +0200

    Added: - show all images - download images - delete images - fancybox image gallery

--- u21430854_HW03/Controllers/ImagesController.cs
+++ u21430854_HW03/Controllers/ImagesController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.IO;
+using u21430854_HW03.Models;
 
 namespace u21430854_HW03.Controllers
 {
@@ -11,7 +13,35 @@ namespace u21430854_HW03.Controllers
         // GET: Images
         public ActionResult Index()
         {
-            return View();
+            string[] imgPaths = Directory.GetFiles(Server.MapPath("~/Media/Images")); //get file paths
+
+            List<FileModel> images = new List<FileModel>(); //list for image files
+
+            foreach (string imagePath in imgPaths)
+            {
+                images.Add(new FileModel { FileName = Path.GetFileName(imagePath) });
+            }
+
+            return View(images);
+        }
+
+        public FileResult DownloadFile(string name)
+        {
+            //read file into array; use byte array because of octet-stream
+            byte[] byteArray = System.IO.File.ReadAllBytes(Server.MapPath("~/Media/Images/") + name);
+
+            //send file to be downloaded
+            return File(byteArray, "application/octet-stream", name);
+        }
+
+        public ActionResult DeleteFile(string name)
+        {
+            string imgPath = Server.MapPath("~/Media/Images/") + name; //file path
+            byte[] byteArray = System.IO.File.ReadAllBytes(imgPath); //read file
+
+            System.IO.File.Delete(imgPath); //delete file
+
+            return RedirectToAction("Index"); //redirect to default view so that images are reloaded
         }
     }
 }
--- u21430854_HW03/Models/FileModel.cs
+++ u21430854_HW03/Models/FileModel.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations; //for text decorators
 
 namespace u21430854_HW03.Models
 {
     public class FileModel
     {
+        [Display(Name = "File name")]
         public string FileName { get; set; }
 
+        [Display(Name = "File")]
         public HttpPostedFileBase Files { get; set; }
     }
 }
